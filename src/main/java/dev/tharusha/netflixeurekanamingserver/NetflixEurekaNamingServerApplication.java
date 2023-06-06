cafd77a6037948c0c9cbd9654971003bd2087eaa
VWIJAT1 <tharusha.wijayabahu@pearson.com>
2023-06-06 13:44:29 +0530

    Netflix Eureka Naming Server: Lightweight service registry for microservices. Java 11 compatible, scalable, and fault-tolerant.

---
+++ src/main/java/dev/tharusha/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,17 @@
+package dev.tharusha.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class NetflixEurekaNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+    }
+
+}
